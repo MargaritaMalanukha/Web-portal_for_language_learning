2ab94fca74b7ce2d143c3104f392e173ab066b65
MargaritaMalanukha <margomalanuha@gmail.com>
2020-12-24 21:00:13 +0300

    added routes, controllers and templates to them

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Языковой портал</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+ * Routes for authorization/registration.
+ */
+
+Route::get('/login', 'App\Http\Controllers\AuthController@login');
+Route::get('/register', 'App\Http\Controllers\AuthController@register');
+Route::get('/logout', 'App\Http\Controllers\AuthController@logout');
+
+/*
+ * Categories routes.
+ */
+
+Route::get('/home', 'App\Http\Controllers\HomeController@index');
+Route::get('/rooms', 'App\Http\Controllers\RoomController@index');
+Route::get('/tests', 'App\Http\Controllers\TestController@index');
+Route::get('/cabinet', 'App\Http\Controllers\CabinetController@index');
