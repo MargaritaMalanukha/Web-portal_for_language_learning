143f1fef51e7924d82d0caed3da1610aead67e9a
IvanBerezovskiy <bereza.par.ez@gmail.com>
2020-12-27 23:05:39 +0200

    created_testing_wiews

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     public function index() {
-        return view('tests');
+        return view('testing_subsystem.test_Stage1');
     }
 }
